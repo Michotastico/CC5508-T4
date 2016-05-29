9bb26f52ff09790cb6938ef3a8716d40cd27417a
Michel Llorens <mllorens@dcc.uchile.cl>
2016-05-29 13:07:45 -0300

    Refactor in folders. Add new images. Add iterative process

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from algorithm.Homography import Homography
 from skimage import io
 
-point = [[155, 130], [250, 120], [105, 555], [250, 580]]
-point_ = [[145, 120], [242, 120], [145, 480], [242, 480]]
-homography = Homography("Imagenes/im01.jpg", point, point_)
-image = homography.transform_image()
-io.imsave("Imagenes/resultado_1_close.jpg", image)
-image = homography.transform_image("bilineal")
-io.imsave("Imagenes/resultado_1_bilineal.jpg", image)
-
+images = [
+    ["Images/im01.jpg",
+     [[155, 130], [250, 120], [105, 555], [250, 580]],
+     [[145, 120], [242, 120], [145, 480], [242, 480]]]
+]
+for image in images:
+    homography = Homography(image[0], image[1], image[2])
+    img = homography.transform_image()
+    io.imsave(image[0]+"_close.jpg", img)
+    img = homography.transform_image("bilineal")
+    io.imsave(image[0] + "_bilineal.jpg", img)
